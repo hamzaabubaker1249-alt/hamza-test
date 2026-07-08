59385a3966ebf766065a13d8265e0be44674737d
hamzaabubaker1249-alt <hamzaabubaker1249@gmail.com>
2026-07-09 00:28:29 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,31 +1,4 @@
-const CACHE_NAME = 'ahmia-clone-v1';
-const urlsToCache = [
-  './',
-  './index.html',
-  './style.css',
-  './app.js',
-  './manifest.json'
-];
-
-// تثبيت الـ Service Worker وحفظ الملفات في الكاش
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
-  );
+self.addEventListener('install', (e) => {
+    e.waitUntil(caches.open('v1').then(cache => cache.addAll(['./', './index.html', './app.js', './manifest.json'])));
 });
-
-// جلب الملفات (عمل الموقع حتى بدون إنترنت)
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
-  );
-});
+self.addEventListener('fetch', (e) => e.respondWith(caches.match(e.request).then(res => res || fetch(e.request))));
