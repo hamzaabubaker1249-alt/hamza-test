b872b9ad363ddd1fcd593690ba3c4ab5754828f6
hamzaabubaker1249-alt <hamzaabubaker1249@gmail.com>
2026-07-26 13:49:32 +0200

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const btn = document.getElementById('helloBtn');
-const message = document.getElementById('message');
-
-btn.addEventListener('click', () => {
-  message.textContent = 'تم الضغط على الزر بنجاح!';
-});
